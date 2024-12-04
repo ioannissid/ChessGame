1f59a9855c397f64b693c80a3a25e63fcbfef248
ioannissid <51276858+ioannissid@users.noreply.github.com>
2024-12-04 19:50:04 +0200

    MOVES SEMIWORKING (CHAOS)

--- chess engine/Chess/ChessEngine.py
+++ chess engine/Chess/ChessEngine.py
@@ -1,10 +1,10 @@
 #Chess engine to calculate, play moves, save info and valid moves
-class GameState():
+class GAMESTATE():
     def __init__(self):
         #2d list of the board
         #pieces are based on official chess annotation
         #w=white,b=black,R=Rook,N=Knight(to avoid confusion with king),B=Bishop,Q=Queen,K=King, - = empty space
-        self.board = [
+        self.BOARD = [
             ["bR","bN","bB","bQ","bK","bB","bN","bR"],
             ["bP","bP","bP","bP","bP","bP","bP","bP"],
             ["-","-","-","-","-","-","-","-","-",],
@@ -13,5 +13,33 @@ class GameState():
             ["-","-","-","-","-","-","-","-","-",],
             ["wP","wP","wP","wP","wP","wP","wP","wP"],
             ["wR","wN","wB","wQ","wK","wB","wN","wR"]]
-        self.whiteToMove = True
-        self.moveLog=[]
+        self.WHITETOMOVE = True
+        self.MOVELOG=[]
+        
+    def MAKEMOVE(self,MOVE):
+        self.BOARD[MOVE.STARTROW][MOVE.STARTCOL] = "-"
+        self.BOARD[MOVE.ENDROW][MOVE.ENDCOL] = MOVE.PIECEMOV
+        self.MOVELOG.append(MOVE) #Log move
+        self.WHITETOMOVE=not self.WHITETOMOVE #switch turns
+        
+class MOVE(): 
+    #chess dictionary to map keys to values
+    RANKSTOROWS= {"1" : 7, "2":6, "3":5, "4":4,
+                  "5":3,"6":2,"7":1,"8":0}
+    ROWSTORANKS= {i: j for j, i in RANKSTOROWS.items()}
+    FILESTOCOLS={"a":0,"b":1,"c":2,"d":3,
+                 "e":4,"f":5,"g":6,"h":7}
+    COLSTOFILES={i:j for j, i in FILESTOCOLS.items()}
+    def __init__(self,START,END,BOARD): #starting square ,ending square and board state
+        self.STARTROW= START[0] #initial selected row
+        self.STARTCOL= START[1] #initial selected collumn
+        self.ENDROW= END[0] #last selected row
+        self.ENDCOL= END[1] #last selected collumn
+        self.PIECEMOV =BOARD[self.STARTROW][self.STARTCOL]#piece moved
+        self.PIECECAP= BOARD[self.ENDROW][self.ENDCOL]#piece captured
+
+    def GETCHESSNOTATION(self):
+       return self.GETRANKFILE(self.STARTROW,self.STARTCOL) + self.GETRANKFILE(self.ENDROW,self.ENDCOL)
+        
+    def GETRANKFILE(self,RANK,COL):
+        return self.COLSTOFILES[COL]+self.ROWSTORANKS[RANK]
--- chess engine/Chess/MainGame.py
+++ chess engine/Chess/MainGame.py
@@ -1,6 +1,6 @@
 
 #Drive file with user input and displaying the gamestate.
-import pygame
+import pygame as g #shortcut for game cause writing pygame over and over became tedious
 import ChessEngine
 HEIGHT = WIDTH = 640 #pieces are 60x60
 DIMENSION = 8 #board dimensions
@@ -11,45 +11,63 @@ IMAGES = {}
 def LoadImage():
     PIECES= ['wP','wR','wN','wB','wQ','wK','bP','bR','bN','bB','bK','bQ']
     for PIECE in PIECES:
-        IMAGES[PIECE]=pygame.transform.scale(pygame.image.load("chess/Image/"+PIECE+".png"),(SQ_SIZE,SQ_SIZE))
+        IMAGES[PIECE]=g.transform.scale(g.image.load("chess/Image/"+PIECE+".png"),(SQ_SIZE,SQ_SIZE))
 
 #Main driver to deal with user input and graphics
 def main():
-    pygame.init()
-    SCREEN= pygame.display.set_mode((WIDTH,HEIGHT))
-    CLOCK=pygame.time.Clock()
-    SCREEN.fill(pygame.Color("white"))
-    GAMESTATE= ChessEngine.GameState()
+    g.init()
+    SCREEN= g.display.set_mode((WIDTH,HEIGHT))
+    CLOCK=g.time.Clock()
+    SCREEN.fill(g.Color("white"))
+    GAMESTATE= ChessEngine.GAMESTATE()
     LoadImage()# only once
     RUNNING = True
+    SQSELECTED=() # keeps tract  of the user's last click
+    PLAYERCLICKS= [] # keeps tract of the total clicks so it can move the pieces
     while RUNNING:
-        for i in pygame.event.get():
-            if i.type==pygame.QUIT:
+        for i in g.event.get():
+            if i.type==g.QUIT:
                 RUNNING=False
+            elif i.type == g.MOUSEBUTTONDOWN:
+                LOCATION = g.mouse.get_pos()
+                COL = LOCATION[0]//SQ_SIZE
+                ROW = LOCATION[1]//SQ_SIZE
+                if SQSELECTED== (ROW,COL): #same square pick
+                    SQSELECTED =() # deselecting
+                    PLAYERCLICKS= []
+                else:
+                    SQSELECTED= (ROW,COL)
+                    PLAYERCLICKS.append(SQSELECTED)
+                if len(PLAYERCLICKS)==2 : #2nd click
+                    MOVE = ChessEngine.MOVE(PLAYERCLICKS[0],PLAYERCLICKS[1],GAMESTATE.BOARD)
+                    print (MOVE.GETCHESSNOTATION()) # debug
+                    GAMESTATE.MAKEMOVE(MOVE)
+                    SQSELECTED =() #resetting clicks
+                    PLAYERCLICKS= []
         DrawGameState(SCREEN,GAMESTATE)
         CLOCK.tick(MAX_FPS)
-        pygame.display.flip()
+        g.display.flip()
         
 
 #Draw the board and everything else
 def DrawGameState(SCREEN,GAMESTATE): 
     DrawBoard(SCREEN)
-    DrawPieces(SCREEN,GAMESTATE.board)
+    DrawPieces(SCREEN,GAMESTATE.BOARD)
 
 def DrawBoard(SCREEN):
     COLORS=[(247, 203, 161),(77, 36, 0)]
     for i in range(DIMENSION):
         for j in range(DIMENSION):
             COLOR = COLORS[(i + j) % 2] 
-            pygame.draw.rect(SCREEN, COLOR, pygame.Rect(j * SQ_SIZE, i * SQ_SIZE, SQ_SIZE, SQ_SIZE))
-    print("Board drawn")
+            g.draw.rect(SCREEN, COLOR, g.Rect(j * SQ_SIZE, i * SQ_SIZE, SQ_SIZE, SQ_SIZE))
+    #print("Board drawn") debug purpose
     
 def DrawPieces(SCREEN,BOARD):
     for i in range(DIMENSION):
         for j in range(DIMENSION):
             PIECE= BOARD[i][j]
             if PIECE != "-": #not empty
-               SCREEN.blit(IMAGES[PIECE],pygame.Rect(j*SQ_SIZE,i*SQ_SIZE,SQ_SIZE,SQ_SIZE)) 
+               SCREEN.blit(IMAGES[PIECE],g.Rect(j*SQ_SIZE,i*SQ_SIZE,SQ_SIZE,SQ_SIZE)) 
             
                 
 if __name__=="__main__":
